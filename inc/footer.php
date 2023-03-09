3c9d054326960c40126e43b9e721454f58690b0d
Yeison Roa <alexandr.ortega@hotmail.com>
2023-03-09 15:10:17 -0500

    creating footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <h3 class="text-center my-4 py-3 text-light" id="title">Para más desarrollos accede a <a href="https://www.configuroweb.com/46-aplicaciones-gratuitas-en-php-python-y-javascript/#Aplicaciones-gratuitas-en-PHP,-Python-y-Javascript" target="_blank" style="color:white;text-decoration:none;">ConfiguroWeb</a></h3>
+    <h3 class="text-center my-4 py-3 text-light" id="title">Para más desarrollos visita <a href="https://github.com/yaroa58" target="_blank" style="color:white;text-decoration:none;">GitHub</a></h3>
 </footer>
